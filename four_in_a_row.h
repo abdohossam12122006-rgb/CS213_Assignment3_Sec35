5a173cf84f98b0fc6098e649822dfab3b6d6804c
abdohossam12122006-rgb <abdohossam12122006@gmail.com>
2025-11-26 01:57:39 +0200

    Update four_in_a_row.h

--- four_in_a_row.h
+++ four_in_a_row.h
@@ -1,27 +1,88 @@
-#pragma once
-#ifndef FOUR_IN_A_ROW_H
-#define FOUR_IN_A_ROW_H
-#include "BoardGame_Classes.h" 
+#ifndef FOUR_IN_A_ROW_BOARD_H
+#define FOUR_IN_A_ROW_BOARD_H
+
+#include "BoardGame_Classes.h"
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
-class Connect4Board :public Board<char>
-{
-private:
-    char blank_symbol = '.';
+class Four_in_a_row_Board : public Board<char> {
 public:
-    Connect4Board();
-    bool update_board(Move<char>* move);
-    bool is_win(Player<char>* player);
-    bool is_lose(Player<char>* player);
-    bool is_draw(Player<char>* player);
-    bool game_is_over(Player<char>* player);
-};
-class Connect4UI : public UI<char> {
-public:
-    Connect4UI();
-    ~Connect4UI() {};
-    Player<char>* create_player(string& name, char symbol, PlayerType type);
-    virtual Move<char>* get_move(Player<char>* player);
+    Four_in_a_row_Board() : Board<char>(6, 7) {
+        this->n_moves = 0;
+        for (int i = 0; i < this->rows; i++) {
+            for (int j = 0; j < this->columns; j++) {
+                this->board[i][j] = 0;
+            }
+        }
+    }
+
+    bool update_board(Move<char>* move) override {
+        int y = move->get_y();
+
+        if (y < 0 || y >= this->columns) {
+            return false;
+        }
+
+        int validRow = -1;
+        for (int i = this->rows - 1; i >= 0; i--) {
+            if (this->board[i][y] == 0) {
+                validRow = i;
+                break;
+            }
+        }
+
+        if (validRow == -1) {
+            return false;
+        }
+        this->board[validRow][y] = move->get_symbol();
+        this->n_moves++;
+        return true;
+    }
+
+    bool is_win(Player<char>* p) override {
+        char symbol = p->get_symbol(); 
+
+        for (int i = 0; i < this->rows; i++) {
+            for (int j = 0; j < this->columns; j++) {
+                if (this->board[i][j] != symbol) continue;
+
+                if (j + 3 < this->columns &&
+                    this->board[i][j + 1] == symbol &&
+                    this->board[i][j + 2] == symbol &&
+                    this->board[i][j + 3] == symbol) return true;
+
+                if (i + 3 < this->rows &&
+                    this->board[i + 1][j] == symbol &&
+                    this->board[i + 2][j] == symbol &&
+                    this->board[i + 3][j] == symbol) return true;
+
+                if (i + 3 < this->rows && j + 3 < this->columns &&
+                    this->board[i + 1][j + 1] == symbol &&
+                    this->board[i + 2][j + 2] == symbol &&
+                    this->board[i + 3][j + 3] == symbol) return true;
+
+                if (i + 3 < this->rows && j - 3 >= 0 &&
+                    this->board[i + 1][j - 1] == symbol &&
+                    this->board[i + 2][j - 2] == symbol &&
+                    this->board[i + 3][j - 3] == symbol) return true;
+            }
+        }
+        return false;
+    }
+
+    bool is_lose(Player<char>* p) override {
+        return false;
+    }
+
+    bool is_draw(Player<char>* p) override {
+        return (this->n_moves == 42 && !is_win(p));
+    }
+
+    bool game_is_over(Player<char>* p) override {
+        return is_win(p) || is_draw(p);
+    }
 };
 
 #endif
