8884825ba6bfa4dc1e274debc2c191d72e125537
ManalEzzat <manalezzat590@gmail.com>
2025-12-11 00:14:48 +0200

    Update SUS_Board.h
    AI UPDATE

--- SUS_Board.h
+++ SUS_Board.h
@@ -1,82 +1,106 @@
-#ifndef SUS_BOARD_H
-#define SUS_BOARD_H
 
-#include "BoardGame_Classes.h"
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include "SUS_UI.h"
+#include <cctype>
 
-using namespace std;
+SUS_Board::SUS_Board() : Board(3, 3) {
+    for (auto& row : board)
+        for (auto& c : row)
+            c = blank_symbol;
+}
 
-class SUS_Board : public Board<char> {
-private:
-    int player_scores[2]; 
+bool SUS_Board::update_board(Move<char>* move) {
+    int x = move->get_x();
+    int y = move->get_y();
+    char mark = move->get_symbol();
 
-public:
-    SUS_Board() : Board<char>(3, 3) {
-        this->n_moves = 0;
-        player_scores[0] = 0;
-        player_scores[1] = 0;
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++) this->board[i][j] = 0;
-    }
+    if (x < 0 || x >= rows || y < 0 || y >= columns)
+        return false;
 
-    int count_new_sus(int r, int c) {
-        char current = this->board[r][c];
-        int count = 0;
-        int dr[] = { 0, 0, 1, -1, 1, -1, 1, -1 };
-        int dc[] = { 1, -1, 0, 0, 1, -1, -1, 1 };
+    if (board[x][y] != blank_symbol)
+        return false;
 
-        if (current == 'U') {
-            for (int i = 0; i < 8; i += 2) {
-                int r1 = r + dr[i], c1 = c + dc[i];
-                int r2 = r + dr[i + 1], c2 = c + dc[i + 1];
-                if (is_valid(r1, c1) && is_valid(r2, c2)) {
-                    if (this->board[r1][c1] == 'S' && this->board[r2][c2] == 'S') count++;
-                }
+    board[x][y] = toupper(mark);
+    n_moves++;
+    return true;
+}
+
+int SUS_Board::count_sequences(char symbol) {
+    int count = 0;
+    int dir[4][2] = { {1,0}, {0,1}, {1,1}, {1,-1} };
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            for (auto& d : dir) {
+                int x1 = i, y1 = j;
+                int x2 = i + d[0], y2 = j + d[1];
+                int x3 = i + 2 * d[0], y3 = j + 2 * d[1];
+
+                if (x3 < 0 || x3 >= rows || y3 < 0 || y3 >= columns) continue;
+
+                if (board[x1][y1] == 'S' && board[x2][y2] == 'U' && board[x3][y3] == 'S')
+                    count++;
             }
         }
-        else if (current == 'S') {
-            for (int i = 0; i < 8; i++) {
-                int r1 = r + dr[i], c1 = c + dc[i];
-                int r2 = r + dr[i] * 2, c2 = c + dc[i] * 2;
-                if (is_valid(r1, c1) && is_valid(r2, c2)) {
-                    if (this->board[r1][c1] == 'U' && this->board[r2][c2] == 'S') count++;
-                }
+    }
+    return count;
+}
+
+bool SUS_Board::is_draw(Player<char>* player) {
+    return (n_moves == 9);
+}
+
+bool SUS_Board::game_is_over(Player<char>* player) {
+    return n_moves == 9;
+}
+
+int SUS_Board::count_sequences_in_matrix(const std::vector<std::vector<char>>& mat, char symbol) {
+    int rows = (int)mat.size();
+    if (rows == 0) return 0;
+    int cols = (int)mat[0].size();
+    int count = 0;
+    int dir[4][2] = { {1,0}, {0,1}, {1,1}, {1,-1} };
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            for (auto& d : dir) {
+                int x1 = i, y1 = j;
+                int x2 = i + d[0], y2 = j + d[1];
+                int x3 = i + 2 * d[0], y3 = j + 2 * d[1];
+
+                if (x3 < 0 || x3 >= rows || y3 < 0 || y3 >= cols) continue;
+
+                if (mat[x1][y1] == 'S' && mat[x2][y2] == 'U' && mat[x3][y3] == 'S')
+                    count++;
             }
         }
-        return count;
+    }
+    return count;
+}
+
+int SUS_AI::evaluate(const std::vector<std::vector<char>>& mat, char me) {
+    int myScore = SUS_Board::count_sequences_in_matrix(mat, me);
+    char opponent = (me == 'S') ? 'U' : 'S';
+    int oppScore = SUS_Board::count_sequences_in_matrix(mat, opponent);
+    return myScore * 2 - oppScore;
+}
+
+// setup_players implementation
+Player<char>** SUS_UI::setup_players() {
+    Player<char>** players = new Player<char>*[2];
+    string name, type;
+
+    for (int i = 0; i < 2; i++) {
+        cout << "Enter Player " << (i == 0 ? 'S' : 'U') << " name: ";
+        cin >> name;
+
+        cout << "Choose type (Human/AI): ";
+        cin >> type;
+
+        if (type == "AI" || type == "ai")
+            players[i] = new SUS_AI(name, i == 0 ? 'S' : 'U');
+        else
+            players[i] = new Player<char>(name, i == 0 ? 'S' : 'U', PlayerType::HUMAN);
     }
 
-    bool is_valid(int r, int c) {
-        return r >= 0 && r < 3 && c >= 0 && c < 3;
-    }
-
-    bool update_board(Move<char>* move) override {
-        int x = move->get_x();
-        int y = move->get_y();
-        if (x < 0 || x >= 3 || y < 0 || y >= 3 || this->board[x][y] != 0) return false;
-
-        this->board[x][y] = move->get_symbol();
-
-        int points = count_new_sus(x, y);
-        int currentPlayerIndex = this->n_moves % 2;
-        player_scores[currentPlayerIndex] += points;
-
-        this->n_moves++;
-        return true;
-    }
-
-    int get_score(int playerIndex) {
-        if (playerIndex >= 0 && playerIndex < 2)
-            return player_scores[playerIndex];
-        return 0;
-    }
-
-    bool is_win(Player<char>* player) override { return false; }
-    bool is_lose(Player<char>* player) override { return false; }
-    bool is_draw(Player<char>* player) override { return (this->n_moves == 9); }
-    bool game_is_over(Player<char>* player) override { return (this->n_moves == 9); }
-};
-
-#endif
+    return players;
+}
